cfda8d9afa3111c08f1fe182fd9d96d9eb88884b
Charles-Elie Hervy <75300601+chervys@users.noreply.github.com>
2023-12-15 16:34:07 +0100

    Impl trim_whitespace, ...

--- inc/string.hpp
+++ inc/string.hpp
@@ -6,7 +6,7 @@
 /*   By: chervy <chervy@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 16:13:23 by chervy            #+#    #+#             */
-/*   Updated: 2023/12/15 16:32:10 by chervy           ###   ########.fr       */
+/*   Updated: 2023/12/15 16:32:37 by chervy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,26 @@
 
 namespace ft
 {
+    int is_digit(char c)
+    {
+        return c >= '0' && c <= '9';
+    }
+    
+    int is_white_space(char c)
+    {
+        return (c >= 9 && c <= 13) || c == 32;
+    }
 
+    void trim_whitespace(std::string &str)
+    {
+        std::string::iterator it = str.begin();
+        while (it != str.end()) {
+            if (is_white_space(*it))
+                str.erase(it);
+            else
+                it++;
+        }
+    }
 }
 
 #endif
