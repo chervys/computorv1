e331d5f59345eb665f2899f69a9c57b88b0c9039
Charles-Elie Hervy <75300601+chervys@users.noreply.github.com>
2024-01-08 14:49:13 +0100

    Clean & Fix

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chervy <chervy@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 14:38:53 by chervy            #+#    #+#             */
-/*   Updated: 2024/01/08 13:57:37 by chervy           ###   ########.fr       */
+/*   Updated: 2024/01/08 14:30:36 by chervy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,52 +18,17 @@ int main(int argc, char** argv)
 {
     (void)argc;
     (void)argv;
-    std::cout << std::endl;
 
-    /* Test math.hpp */
-    std::cout << "### Test math.hpp ###" << std::endl;
-    {
-        double x = -3.14;
-        std::cout << "sqrt(x)       =	" << std::sqrt(x) << std::endl;
-        std::cout << "ft_sqrt(x)    =	" << ft::sqrt(x) << std::endl;
+    if (argc != 2) {
+        std::cout << "Invalid arguments." << std::endl;
+        return -1;
     }
-    {
-        double x = 3.14;
-        std::cout << "sqrt(x)       =	" << std::sqrt(x) << std::endl;
-        std::cout << "ft_sqrt(x)    =	" << ft::sqrt(x) << std::endl;
+
+    try {
+        ft::polynomial(std::string(argv[1]));
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
     }
-    std::cout << std::endl;
-
-    /* Test polynomial.hpp */
-    std::cout << "### Test polynomial.hpp ###" << std::endl;
-    {
-        try {
-            ft::polynomial("50000000 * X^0 + 4 * X^1 - 3 * X^2 = 5 * X^0");
-            std::cout << std::endl;
-
-            ft::polynomial("1 * X^0 + 4 * X^1 + 4 * X^2 = 0 * X^0");
-            std::cout << std::endl;
-
-            ft::polynomial("3 * X^0 + 4 * X^1 + 4 * X^2 = 0 * X^0");
-            std::cout << std::endl;
-
-            ft::polynomial("5 * X^0 + 4 * X^1 = 5 * X^0");
-            std::cout << std::endl;
-
-            ft::polynomial("5 * X^0 + 4 * X^1 = 0 * X^0");
-            std::cout << std::endl;
-
-            ft::polynomial("5 * X^3 = 0 * X^0");
-            std::cout << std::endl;
-
-            ft::polynomial("5 * X^-42 = 0 * X^0");
-            std::cout << std::endl;
-
-        } catch (const std::exception& e) {
-            std::cerr << e.what() << '\n';
-        }
-    }
-    std::cout << std::endl;
 
     return 0;
 }
